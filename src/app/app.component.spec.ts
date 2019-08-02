a2aa3b36381548135ea9e6229e99c9ffa5c98da0
Praveen Padma <prpadma@deloitte.com>
2019-08-02 13:00:19 -0400

    Passing broken test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'praveen-pbp-app'`, () => {
+  it(`should have as title 'Praveen!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('praveen-pbp-app');
+    expect(app.title).toEqual('Praveen!');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to praveen-pbp-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Praveen!!');
   });
 });
